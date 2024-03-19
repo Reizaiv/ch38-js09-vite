241bf9772fe27f481cfb96dc01158a07daa6be49
Javier Villagomez <javier_diaz16@hotmail.com>
2024-03-19 12:10:21 -0600

    Hecho el ejercicio con tarjetas

---
+++ src/js/getUsers.js
@@ -0,0 +1,35 @@
+import axios from "axios";
+
+const getUsersUsingAxios = async( url ) =>{
+    try {
+        const response = await axios.get(url);
+        console.log(response);
+        const users = response.data.data;
+        return users; // Axios realiza una conversión implícita de json a Object
+    } catch (error) {
+        
+    }
+}
+
+
+const getUsersUsignFetch = async( url ) => {
+    
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        const users = data.data;
+        return users;
+        
+    } catch (error) {
+        console.error( error );
+    }
+
+}
+
+export {getUsersUsignFetch, getUsersUsingAxios};
+
+const tableUsers = async (users) => {
+    console.table(users);
+}
+
+export {tableUsers};
